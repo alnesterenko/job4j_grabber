108409d6c7b0c46a7c3a8341f658f3fba13d6f9a
alnesterenko <alzpupkin@gmail.com>
2024-07-19 05:52:39 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -9,13 +9,32 @@ import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.util.List;
 
 public class HabrCareerParse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
-    public static final int PAGE_LIMITER = 5;
+    public static final int PAGE_LIMITER = 1; /*Не забыть вернуть назад 5*/
+
+    private String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(link);
+        Document document = connection.get();
+        Elements rows = document.select(".faded-content__body");
+        return cutterOfHTMLTags(rows.get(0));
+    }
+
+    private String cutterOfHTMLTags(Element element) {
+        String tempString = element.toString();
+        tempString = tempString.replaceAll(
+                "<strong>|</strong>|<ul>|</ul>|<br>|<div.*?>|</div>|<h\\d+?>|<p>|</h\\d+?>|</p>|</li>| {2,4}",
+                "");
+        tempString = tempString.replaceAll("&nbsp;", " ");
+        tempString = tempString.replaceAll("\\s{2,}", System.lineSeparator());
+        tempString = tempString.replaceAll("<li>", " - ");
+        return tempString;
+    }
 
     public static void main(String[] args) throws IOException {
         for (int i = 1; i <= PAGE_LIMITER; i++) {
@@ -34,5 +53,7 @@ public class HabrCareerParse {
                         formatter.parse(timeElement.attr("datetime")));
             });
         }
+        HabrCareerParse testParse = new HabrCareerParse();
+        System.out.println(testParse.retrieveDescription("https://career.habr.com/vacancies/1000139737"));
     }
 }
